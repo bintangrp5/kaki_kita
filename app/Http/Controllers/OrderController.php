bd89f77b978ceb33f5b13b142b3dd12320ed0819
bintangrp5 <bintangrafli73@gmail.com>
2025-07-17 17:40:31 +0700

    Update Final 17/07 #34

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -10,10 +10,22 @@ class OrderController extends Controller
     public function index()
     {
         $user = auth()->guard('customer')->user();
-        $orders = Order::where('customer_id', $user->id)->latest()->get();
 
+        if (!$user) {
+            return redirect()->route('customer.login')->with('error', 'Silakan login terlebih dahulu.');
+        }
+
+        $orders = Order::where('customer_id', $user->id)->latest()->get();
         return view('web.order', compact('orders'));
     }
+
+    // public function index()
+    // {
+    //     $user = auth()->guard('customer')->user();
+    //     $orders = Order::where('customer_id', $user->id)->latest()->get();
+
+    //     return view('web.order', compact('orders'));
+    // }
 
     public function show($id)
     {
