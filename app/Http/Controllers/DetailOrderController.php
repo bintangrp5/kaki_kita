777ee5c992271ae2a647c50bb8911c942ad76779
bintangrp5 <bintangrafli73@gmail.com>
2025-07-17 15:33:17 +0700

    Final Update 17/07 #16

--- app/Http/Controllers/DetailOrderController.php
+++ app/Http/Controllers/DetailOrderController.php
@@ -48,6 +48,6 @@ class DetailOrderController extends Controller
     {
         $order = \App\Models\Order::with(['details.product'])->findOrFail($id);
 
-        return view('admin.order_detail', compact('order'));
+        return view('web.order_detail', compact('order'));
     }
 }
